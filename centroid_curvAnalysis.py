13f2022c95ebfd0f251403f791942191f6daf657
Rudolph Pienaar <rudolph.pienaar@gmail.com>
2014-01-08 11:41:53 -0500

    Fix some issues with percentage density calculation.

--- centroid_curvAnalysis.py
+++ centroid_curvAnalysis.py
@@ -446,12 +446,14 @@ class FNNDSC_CentroidCloud(base.FNNDSC):
         For a given polygon and a set of shapely points in the same space, 
         return the subset of points that are contained within the polygon 
         as well as the "density".
+        
+        Density is returned as a percentage.
         '''
         l_pointWithin   = []
         for pt in apnt:
             if aplgn_space.contains(pt):
                 l_pointWithin.append(pt)
-        f_d = float(len(l_pointWithin)) / float(len(apnt))
+        f_d = float(len(l_pointWithin)) / float(len(apnt)) * 100
         return l_pointWithin, f_d
 
     def groupIntersections_determine(self, **kwargs):
@@ -541,7 +543,7 @@ class FNNDSC_CentroidCloud(base.FNNDSC):
         misc.file_writeOnce('%s-cloudCoreOverlapAreaDensity.txt' % \
                                 (_str_fileName), 
                             '%10.3f%10.3f%10.3f\n' % \
-                                (f_or, f_ol, f_or * f_ol * 100))
+                                (f_or, f_ol, f_or * f_ol / 100))
 
         # Particulate density...
         l_pntr, f_dr  = FNNDSC_CentroidCloud.groupIntersections_pointMembership_find(p_overlap, pnts1)
@@ -552,7 +554,7 @@ class FNNDSC_CentroidCloud(base.FNNDSC):
         misc.file_writeOnce('%s-cloudCoreOverlapParticleDensity.txt' % \
                                 (_str_fileName), 
                             '%10.3f%10.3f%10.3f\n' % \
-                                (f_dr, f_dl, f_dr * f_dl * 100))
+                                (f_dr, f_dl, f_dr * f_dl / 100))
          
 
     def groupTtest_determine(self, **kwargs):
